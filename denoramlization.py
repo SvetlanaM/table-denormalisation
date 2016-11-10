e83ad01a09a07793c2320c3622115c81e34fe741
SvetlanaM <svetlana@margetova.eu>
2016-11-10 10:52:23 +0100

    code update

--- denoramlization.py
+++ denoramlization.py
@@ -1,22 +1,37 @@
 import csv
+import ast
+import os, glob
 
-file_name = input("Add name of the csv: ")
+#final table rows
+rows = []
 
+# mapping variant values
+variants = []
 
-with open(file_name + '.csv', 'r') as infile, open(file_name + '-out.csv', 'w') as outfile:
+#csv header
+headers = ["document_id", "variant_id"]
+
+with open("./data/in/tables/*.csv") as infile, open("./data/in/tables/*.-out.csv") as outfile:
     reader = csv.reader(infile)
     writer = csv.writer(outfile)
 
+    writer.writerow(headers)
     for index_row, data_in_row in enumerate(reader):
         for index_col, data_in_cell in enumerate(data_in_row):
             if index_row > 0:
+                rows.append(data_in_row[index_col])
                 if index_col == 7:
-                    for item in data_in_row[7]:
-                        writer.writerow(data_in_row)
+                    try:
+                        variants = ast.literal_eval(data_in_row[7])
+                    except:
+                        pass
 
-        if index_row > 15:
-            break
+        #remove duplicates
+        variants = dict.fromkeys(variants).keys()
+        for variant in variants:
+            writer.writerow([data_in_row[1], variant])
 
-    print (" First conversion is done ")
+
+    print ("========= Creating the asociated table is done ========")
     infile.close()
     outfile.close()
